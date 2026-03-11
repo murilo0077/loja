aef09950d7f64cac1d19e839704f2dcaeb4eeed4
murilo0077 <murilo.oliveira23@escola.pr.gov.br>
2026-03-11 12:25:02 -0300

    Update loja.py

--- loja.py
+++ loja.py
@@ -1,38 +1,49 @@
-produtos = ["Mouse", "Teclado", "Monitor", "Fone", "Pendrive", "Cabo HDMI", "Mousepad", "Webcam"]
-precos = [50.0, 120.0, 800.0, 150.0, 30.0, 25.0, 40.0, 200.0]
+/*
+Imagine que você abriu uma loja:
+1) Crie o nome de 8 produtos e armazene em uma lista;
+2) Crie outra lista para armazenar o preço de cada produto.
+3) Crie uma função para mostrar o estoque, mostrando o nome da cada produto e o seu preço.
+4) Crie uma função que adiciona um novo produto (e consequentemente um novo preço)
+5) Crie uma função que remove um produto da lista recebendo seu índice.
+6) EXECUTE AS FUNÇÕES CRIADAS NA SEGUINTE ORDEM:
+    - mostra estoque
+    - adiciona produto
+    - mostra estoque
+    - remove um elemento pelo índice
+    - mostra estoque
+*/
 
+const produtos = ["Mouse", "Teclado", "Monitor", "Fone", "Pendrive", "Cabo HDMI", "Mousepad", "Webcam"]
+const precos = [50, 120, 800, 150, 30, 25, 40, 200]
 
-def mostrar_estoque():
-    print("--- ESTOQUE ---")
-    print("0:", produtos[0], "| R$", precos[0])
-    print("1:", produtos[1], "| R$", precos[1])
-    print("2:", produtos[2], "| R$", precos[2])
-    print("3:", produtos[3], "| R$", precos[3])
-    print("4:", produtos[4], "| R$", precos[4])
-    print("5:", produtos[5], "| R$", precos[5])
-    print("6:", produtos[6], "| R$", precos[6])
-    print("7:", produtos[7], "| R$", precos[7])
+function mostrarEstoque() {
+    let contador = 0
+    console.log("--- ESTOQUE ATUAL ---")
+    while (contador < produtos.length) {
+        console.log(produtos[contador] + " - R$ " + precos[contador])
+        contador++
+    }
+    console.log("--------------------")
+}
 
-    if len(produtos) > 8:
-        print("8:", produtos[8], "| R$", precos[8])
-    print("-------------------")
+function adicionarProduto(nome, preco) {
+    produtos.push(nome)
+    precos.push(preco)
+    console.log("Produto adicionado: " + nome + " | Valor: R$ " + preco)
+}
 
-def adicionar_produto(nome, preco):
-    produtos[:] = produtos + [nome]
-    precos[:] = precos + [preco]
-    print("Adicionado com sucesso!")
+function removerProduto(indice) {
+    console.log("Removido o item: " + produtos[indice])
+    produtos.splice(indice, 1)
+    precos.splice(indice, 1)
+}
 
-def remover_produto(indice):
-    del produtos[indice]
-    del precos[indice]
-    print("Item removido!")
+mostrarEstoque()
 
-mostrar_estoque()
+adicionarProduto("Microfone", 180)
 
-adicionar_produto("Microfone", 180.0)
+mostrarEstoque()
 
-mostrar_estoque()
+removerProduto(4)
 
-remover_produto(4)
-
-mostrar_estoque()
+mostrarEstoque()
